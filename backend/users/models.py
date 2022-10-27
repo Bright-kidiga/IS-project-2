01e4b9121744ba8c15296523bb38c15273f23847
bright kidiga <brightkidiga@gmail.com>
2022-10-27 16:07:08 +0300

    'update'

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,22 +8,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUs
 
 
 # Create your models here.
-# class User(AbstractUser):
-#     name = models.CharField(max_length=255)
-#     email = models.EmailField(max_length=255, unique=True)
-#     password = models.CharField(max_length=255)
-#     is_caregiver = models.BooleanField(default=False)
 
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS =['name']
-
-
-
-# class Tasks(models.Model):
-#     name = models.CharField(max_length=255)
-#     users =models.ForeignKey(User)
-#     Approval = models.BooleanField(default=False)
 class UserAccountManager(BaseUserManager):
     def create_user(self,email,name,password=None):
         if not email:
