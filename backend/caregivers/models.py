6abf0945ec4140a23ea489e3ccfbaca039a54318
bright kidiga <brightkidiga@gmail.com>
2022-10-31 16:47:59 +0300

    'caregivers'

--- backend/caregivers/models.py
+++ backend/caregivers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from pickle import TRUE
+from users.models import is_caregiver
 
 # Create your models here.
+def nameFile(instance, filename):
+    return '/'.join(['images', str(instance.name), filename])
+
+class Application():
+    is_cg = models.ForeignKey(is_caregiver, on_delete=models.CASCADE)
+    id_photo = models.ImageField(upload_to= nameFile, blank=True, null=True)
+    is_approved = models.BooleanField(default=False)
--- backend/users/models.py
+++ backend/users/models.py
@@ -53,6 +53,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     is_caregiver = models.BooleanField(default=False)
+    is_approved  = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
